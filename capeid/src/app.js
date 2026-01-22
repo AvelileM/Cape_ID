aaf3f41896a475f2699de52ecdb1c5c72fbad80a
Avelile Mnyazana <avmnycpt025@student.wethinkcode.co.za>
2026-01-22 20:31:46 +0200

    initial

---
+++ capeid/src/app.js
@@ -0,0 +1,17 @@
+// app.js
+import express from "express";
+import path from "path";
+
+const app = express();
+
+// Serve frontend
+app.use(express.static(path.join(process.cwd(), "public")));
+
+// Routes
+import uploadRoute from "./routes/upload.js";
+import verifyRoute from "./routes/verify.js";
+
+app.use(uploadRoute);
+app.use(verifyRoute);
+
+export default app;
